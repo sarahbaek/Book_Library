97e50bb03a10acee80a431b1d3a60b1e57fad9d7
sarahBæk2021 <sbaek18@gmail.com>
2021-10-05 19:22:56 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Book_Library
+{
+    public class Book
+    {
+        /// <summary>
+        /// The title of the book
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// The Author of the book
+        /// </summary>
+        public string Author { get; set; }
+        /// <summary>
+        /// The number of Pages in the book
+        /// </summary>
+        public int  PageNumber { get; set; }
+        /// <summary>
+        /// The unique identifier of the book book
+        /// </summary>
+        public string ISBN13 { get; set; }
+    }
+}
